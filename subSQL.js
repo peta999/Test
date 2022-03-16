fa3aca69eecfa1c254c154413f13e0e9a31bf94e
peta999 <101709235+peta999@users.noreply.github.com>
2022-03-16 17:51:40 +0100

    File für mySQL erstellt

---
+++ subSQL.js
@@ -0,0 +1,14 @@
+const mqtt = require('mqtt')
+const mysql = require('mysql');
+const client = mqtt.connect('mqtt://192.168.2.54:1883')
+const topic = "data"
+
+
+
+
+client.on('message', (topic, message)=>{    
+    console.log(message.toString());    
+});
+client.on('connect', ()=>{
+    client.subscribe(topic);
+});
