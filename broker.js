b36b119c7d603e54e60a6fdf0736dd11690d6f92
peta999 <101709235+peta999@users.noreply.github.com>
2022-03-16 17:45:03 +0100

    !

--- broker.js
+++ broker.js
@@ -6,70 +6,15 @@ server.listen(port, function () {
   console.log('server started and listening on port ', port)
 })
 
-  server.on('message', (package)=> {
-    console.log(package.toString())
-  })
-  server.on('published', (package)=> {
-      console.log(package.toString())
-  })
-  server.on('publish', async function (packet, client) {
-    if (client) {
-        console.log("test")
-    }
-  })
-
-'use strict'
- 
-var fs = require('fs')
-var path = require('path')
-var http = require('http')
-var https = require('https')
-var websocket = require('websocket-stream')
-var net = require('net')
-var tls = require('tls')
-var logging = require('aedes-logging')
-var instance = aedes()
- 
-var servers = [
-  startHttp(),
-  startHttps(),
-  startNet(),
-  startTLS()
-]
- 
-logging({
-  instance: instance,
-  servers: servers
+server.on('message', (package)=> {
+  console.log(package.toString())
 })
- 
-function startHttp () {
-  var server = http.createServer()
-  websocket.createServer({
-    server: server
-  }, instance.handle)
-  server.listen(8880)
-  return server
-}
- 
-function startHttps () {
-  var server = https.createServer({
-    key: fs.readFileSync(path.join(__dirname, 'certs', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'certs', 'cert.pem'))
-  })
-  websocket.createServer({
-    server: server
-  }, instance.handle)
-  server.listen(8881)
-  return server
-}
- 
-function startNet () {
-  return net.createServer(instance.handle).listen(8882)
-}
- 
-function startTLS () {
-  return tls.createServer({
-    key: fs.readFileSync(path.join(__dirname, 'certs', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'certs', 'cert.pem'))
-  }, instance.handle).listen(8883)
-}
+server.on('published', (package)=> {
+    console.log(package.toString())
+})
+server.on('publish', async function (packet, client) {
+  if (client) {
+      console.log("test")
+  }
+})
+
